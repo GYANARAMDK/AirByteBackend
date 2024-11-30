b0db635ef7b8feffe54ac90e433f6048c935d4db
GYANARAM  DHAKA <162135600+GYANARAMDK@users.noreply.github.com>
2024-11-30 22:26:21 +0530

    Update 3verifyauthentication.js

--- Controler/Middlewares/verifyauthentication.js
+++ Controler/Middlewares/verifyauthentication.js
@@ -1,7 +1,7 @@
 const jwt=require('jsonwebtoken')
 const verifyAuthentication= (req,res,next)=>{
     // const authHeader=req.headers.authorization;
-    const authHeader=req.header['authorization'];
+    const authHeader=req.headers['authorization'];
     if(authHeader){
         const token = authHeader.split(' ')[1];
         jwt.verify(token,process.env.JWT_SECRET_KEY,(error,user)=>{
