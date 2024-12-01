d472d533b301ad4053d99a82589800ee47603bbe
“[Gyanaram <[gyanaramnitian@gmail.com]>
2024-12-01 23:45:47 +0530

    new razorpayid added to the database's order

--- Controler/OrderPostControler.js
+++ Controler/OrderPostControler.js
@@ -31,40 +31,62 @@
 
 const razorpay = require('razorpay')
 const User = require('../Model/UserModel')
+const Order = require('../Model/OrderModel')
 const razorpayIntance = require('../razorpayconfig');
 const orderpostcontroler = async (req, res) => {
 
     const userId = req.user.id;
     try {
+        const { shippingAddress } = req.body;
         const user = await User.findById(userId).populate('CartArray.productId');
         if (!user || !user.CartArray || user.CartArray.length === 0) {
             return res.status(400).json({ error: 'Cart is empty or invalid userId' });
         }
 
-        const totalamount= user.CartArray.reduce((acc,item) =>{
-            return acc+(item.price*item.quantity)
-        },0)
+         // calculate total prize for order
+        const totalPrice = user.CartArray.reduce((acc, item) => {
+            return acc + (item.price * item.quantity)
+        }, 0)
 
-        const options={
-            totalamount :totalamount*100,
+        // create the order in the database
+        const orders = new Order({
+            userid: userId,
+            products: user.CartArray.map(item => ({
+                productId: item.productId,
+                quantity: item.quantity,
+                price: item.price,
+            })),
+            totalPrice,
+            shippingAddress,
+            status: 'Pending',
+            paymentStatus: 'Pending',
+        });
+
+        //save the order in the database
+        
+        await orders.save();
+
+        //razorpay options 
+        const options = {
+            totalamount: totalPrice * 100,
             currency: 'INR',
-            receipt: `receipt_${Date.now()}`,
+            receipt: `receipt_${orders._id}`,
         }
-        const order = await new Promise((resolve, reject) => {
+        const razorpayorder = await new Promise((resolve, reject) => {
             razorpayIntance.orders.create(options, (err, order) => {
-              if (err) reject(err);
-              else resolve(order);
+                if (err) reject(err);
+                else resolve(order);
             });
-          });
+        });
         res.status(201).json({
             success: true,
-            order,
+            razorpayorder,
             message: 'Order created successfully',
-          });
+        });
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'Internal Server Error' });
     }
 }
 
-module.exports =orderpostcontroler;
+module.exports = orderpostcontroler;
