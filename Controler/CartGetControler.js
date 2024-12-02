2766f414c9052e402ad3622b919dc6de515f77e1
GYANARAM  DHAKA <162135600+GYANARAMDK@users.noreply.github.com>
2024-12-02 12:31:35 +0530

    Updatee CartGetControler.js

--- Controler/CartGetControler.js
+++ Controler/CartGetControler.js
@@ -10,15 +10,15 @@ const CartGetControler = async (req, res) => {
         })
         
         if (user.CartArray.length === 0) {
-            res.status(404).json({ message: "cart is empty" });
-        }else{
-        res.status(200).json({
+             return res.status(404).json({ message: "cart is empty" });
+        }
+        return res.status(200).json({
             message: 'Cart retrieved successfully.',
             cart: user.CartArray,
-        })};
+        });
     } catch (error) {
         console.error('Error fetching cart:', error);
-        res.status(500).json({ message: 'Something went wrong while fetching the cart.' });
+        return res.status(500).json({ message: 'Something went wrong while fetching the cart.' });
 
     }
 
